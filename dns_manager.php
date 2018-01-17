6c04c329f6147b7d22b43e4c90d8ec82c5646165
Joe Huss <detain@interserver.net>
2018-01-17 20:11:22 +0000

    replacing old dns manger with new one

--- dns_manager.php
+++ dns_manager.php
@@ -8,44 +8,8 @@
  */
 function dns_manager() {
 	page_title('DNS Manager');
-	$custid = $GLOBALS['tf']->session->account_id;
-	$module = 'default';
-	$db = $GLOBALS['tf']->db;
-	if (isset($GLOBALS['tf']->variables->request['module']) && isset($GLOBALS['modules'][$GLOBALS['tf']->variables->request['module']])) {
-			$module = $GLOBALS['tf']->variables->request['module'];
-			$db = get_module_db($module);
-			//				$custid = get_custid($custid, $module);
-			$GLOBALS['tf']->accounts->set_db_module($module);
-			$GLOBALS['tf']->history->set_db_module($module);
-	}
-	$module = get_module_name($module);
-	$settings = get_module_settings($module);
-	$data = $GLOBALS['tf']->accounts->read($custid);
-
-	if (isset($GLOBALS['tf']->variables->request['new']) && $GLOBALS['tf']->variables->request['new'] == 1 && verify_csrf_referrer(__LINE__, __FILE__)) {
-		if (isset($GLOBALS['tf']->variables->request['ip'])) {
-			$ip = trim($db->real_escape($GLOBALS['tf']->variables->request['ip']));
-			if (isset($GLOBALS['tf']->variables->request['domain']) && trim($GLOBALS['tf']->variables->request['domain']) != '') {
-				$domain = trim($GLOBALS['tf']->variables->request['domain']);
-				$result = add_dns_domain($domain, $ip);
-				add_output($result['status_text']);
-			}
-			if (isset($GLOBALS['tf']->variables->request['domains']) && !in_array(trim($GLOBALS['tf']->variables->request['domains']), ['', 'Domain Names...'])) {
-				$domains = explode("\n", $GLOBALS['tf']->variables->request['domains']);
-				foreach ($domains as $domain) {
-					$domain = trim($domain);
-					if ($domain != '') {
-						$result = add_dns_domain($domain, $ip);
-						add_output($result['status_text']);
-					}
-				}
-			}
-		}
-	}
-	if ($GLOBALS['tf']->ima == 'admin')
-		add_output(render_form('dns_manager', ['acl' => 'view_service']));
-	else
-		add_output(render_form('dns_manager', ['custid' => get_custid($GLOBALS['tf']->session->account_id, 'domains')]));
+	function_requirements('crud_dns_manager');
+	crud_dns_manager();
 	$table = new TFTable;
 	$table->set_title('DNS Servers');
 	$table->add_field('Primary DNS');
